c9dcb347b775a17bf35a56a44499b2d192bdd7c0
Deona Williams <deonawilliams@Deonas-MacBook-Pro.local>
2018-02-23 13:58:41 -0600

    working color changing function

--- designs.js
+++ designs.js
@@ -1,33 +1,7 @@
 // Select color input
-let selectedColor;
-let storedColor;
-let gridHeight;
-let gridWidth;
-let submitValue;
+let selectedColor, storedColor, gridHeight, gridWidth, table, rows;
 
-// When size is submitted by the user, call makeGrid()
-function makeGrid(e) {
-    //prevents the form from submitting and refreshing the page
-    e.preventDefault();
-
-    //create rows based on input from user
-    gridHeight = $("#inputHeight").val();
-    for(let height = 0; height < gridHeight; height++){
-        $("table").append("<tr></tr>");
-    }
-
-    //create columns based on input from user
-    gridWidth = $("#inputWidth").val();
-    for(let width = 0; width < gridWidth; width++){
-        $("tr").append("<td></td>");
-        changeBackground();
-    }
-}
-
-//JS to listen for the submit button being clicked, if so call makeGrid()
-document.getElementById("submitBtn").addEventListener("click", makeGrid);
-
-//function to get the color selected by user
+//JS function to get the color selected by user
 function pickColor() {
     //select color
     selectedColor = document.getElementById("colorPicker");
@@ -36,10 +10,36 @@ function pickColor() {
     storedColor = selectedColor.value;
 }
 
-//change color of td
-function changeBackground(){
-    let cell = $("td");
-    cell.click(function(){
-    cell.css("background-color", storedColor);
-    })
+// When size is submitted by the user, call makeGrid()
+function makeGrid(e) {
+    //prevents the form from submitting and refreshing the page
+    e.preventDefault();
+    
+
+    //create rows based on input from user
+    gridHeight = $("#inputHeight").val();
+    table = $('#pixelCanvas');
+
+    for(let height = 0; height < gridHeight; height++){
+       table.append("<tr></tr>");
+    }
+
+    //create columns based on input from user
+    gridWidth = $("#inputWidth").val();
+    rows = $('tr');
+    
+    for(let width = 0; width < gridWidth; width++){
+        rows.append("<td></td>");
+        changeCellColor();
+    }
 }
+
+
+function changeCellColor(){
+    table.on("click", "td", function() {
+        $(this).css("background-color", storedColor);
+    });
+}
+
+//JS to listen for the submit button being clicked, if so call makeGrid()
+document.getElementById("submitBtn").addEventListener("click", makeGrid);
